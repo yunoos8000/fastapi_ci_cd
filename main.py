4c41a51fe913f8bce26453dfa9af21c23d20a862
Yunoos Parvez <yunoos.parvezansari@deorwine.com>
2025-12-29 13:58:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    value_one: int = 0
+    value_two: int = 0
+
+
+items = []
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+def root():
+    return 5
+
+
+@app.post("/items")
+def additional(item: Item):
+    items.append(item)
+    return items
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+app.get("/models/{model_name}")
+
+
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
